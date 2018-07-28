25a8cade22c8d59512b518711b88e4e34049202b
kdncode <kdncode@gmail.com>
2018-07-27 23:44:50 -0500

    added handler

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let express = require('express')
 let app = express()
 
+let aboutHandler = (req, res) => {
+    res.send('New about me by handler')
+}
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
-app.get('/about', (req, res) => {
-    res.send('About me')
-})
+app.get('/about', aboutHandler)
 
 app.listen(8000, () => {
     console.log('Server is running...')
